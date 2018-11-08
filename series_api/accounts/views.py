98bb24b2ef1b4f002fc1ab934d23cc06c814eb15
Unknown <Josrodjr@gmail.com>
2018-11-08 11:53:28 -0600

    Permissions for api/users/

--- series_api/accounts/views.py
+++ series_api/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -9,10 +9,12 @@ from accounts.serializers import UserSerializer
 from django.contrib.auth.models import User
 
 class UserCreate(APIView):
-    # create the user
-    def post(self, request, format='json'):
-      serializer = UserSerializer(data=request.data)
-      if serializer.is_valid():
-        user = serializer.save()
-        if user:
-          return Response(serializer.data, status=status.HTTP_201_CREATED)
+  # Permitir a cualquiera acceder a esta view
+  permission_classes = (AllowAny,)
+  # create the user
+  def post(self, request, format='json'):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+      user = serializer.save()
+      if user:
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
