4138207083e5f64bbe9ff697104a14bfd51c8755
jrmartinez96 <pepemart1996@gmail.com>
2018-11-27 11:20:49 -0600

    Cambio modelo serie de nombre a name

--- series_api/series/models.py
+++ series_api/series/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 
 class Serie(models.Model):
-  nombre = models.CharField(max_length=25)
+  name = models.CharField(max_length=25)
   # Rating solo tiene dos caracteres en los enteros y dos en los decimales
   # total_rating = models.DecimalField(max_digits=2, decimal_places=2)
   total_rating = models.FloatField()
