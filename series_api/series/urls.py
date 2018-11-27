84624ee43b5e98bc3d7733f6f7903a890691b4c0
Unknown <Josrodjr@gmail.com>
2018-11-26 19:13:20 -0600

    add the search by serie_name

--- series_api/series/urls.py
+++ series_api/series/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path('api/series/', views.SerieListCreate.as_view() ),
+    url('^api/series/(?P<serie_name>.+)/$', views.SeriesList.as_view()),
 ]
--- series_api/series/views.py
+++ series_api/series/views.py
@@ -8,3 +8,11 @@ from rest_framework import generics
 class SerieListCreate(generics.ListCreateAPIView):
   queryset = Serie.objects.all()
   serializer_class = SerieSerializer
+
+
+class SeriesList(generics.ListAPIView):
+  serializer_class = SerieSerializer
+
+  def get_queryset(self):
+    seriename = self.kwargs['serie_name']
+    return Series.objects.filter(nombre=seriename)
