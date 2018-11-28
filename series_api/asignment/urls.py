80adf00fb13f3dbe71a98febfa4e1d09e61d2d23
Unknown <Josrodjr@gmail.com>
2018-11-27 21:38:24 -0600

    delete assignment

--- series_api/asignment/urls.py
+++ series_api/asignment/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('api/assignment/', views.AssignmentListCreate.as_view() ),
     url('^api/assignment/(?P<userid>.+)/$', views.AssignmentList.as_view()),
+    path('api/deletion/<int:serieid>/<int:userid>/', views.AssignmentListCreate.delete_assignment ),
+    # url('^api/assignment/(?P<serieid>.+)(?P<userid>.+)/$', views.AssignmentListCreate.delete ),
+    # path('api/deletion//(?P<serieid>\d+)/(?P<userid>\d+)/$', views.AssignmentListCreate.delete_assignment ),
 ]
--- series_api/asignment/views.py
+++ series_api/asignment/views.py
@@ -3,11 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 from asignment.models import Assignment
 from asignment.serializers import AssignmentSerializer
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 class AssignmentListCreate(generics.ListCreateAPIView):
   queryset = Assignment.objects.all()
   serializer_class = AssignmentSerializer
+
+  def delete_assignment(request, userid, serieid):
+    try:
+      Assignment_for_deletion = Assignment.objects.filter(user_id=userid, serie_id=serieid)
+      Assignment_for_deletion.delete()
+      return Response("DELETED SUCCESSFULLY", status=status.HTTP_200_OK)
+    except Assignment.DoesNotExist:
+      return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 class AssignmentList(generics.ListAPIView):
@@ -16,3 +26,20 @@ class AssignmentList(generics.ListAPIView):
   def get_queryset(self):
     userid = self.kwargs['userid']
     return Assignment.objects.filter(user_id=userid)
+
+
+
+#  Esta clase no esta en uso, la eliminacion esta en el delete_assignment de la clase AssignmentListCreate
+class DeleteAssignment(generics.ListAPIView):
+
+  serializer_class = AssignmentSerializer
+
+  def get_queryset(self):
+    userid = self.kwargs['userid']
+    serieid = self.kwargs['serieid']
+    try:
+      Assignment_for_deletion = Assignment.objects.filter(user_id=userid, serie_id=serieid)
+      Assignment_for_deletion.delete()
+      return Response("DELETED SUCCESSFULLY", status=status.HTTP_200_OK)
+    except Assignment.DoesNotExist:
+      return Response(status=status.HTTP_404_NOT_FOUND)
